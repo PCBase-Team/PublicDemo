2b9b2f597ddd35f387c157fa0b7bdb81fe7963cd
hoangle9611 <lvhoang0103@gmail.com>
2025-05-21 17:03:53 +0700

    commit

---
+++ CameraProjectWpf/App.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using Prism.DryIoc;
+using Prism.Ioc;
+
+namespace CameraProjectWpf
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : PrismApplication
+    {
+        protected override Window CreateShell()
+        {
+            var w = Container.Resolve<MainWindow>();
+            return w;
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            if (containerRegistry is null)
+            {
+                throw new ArgumentNullException(nameof(containerRegistry));
+            }
+            //  throw new NotImplementedException();
+        }
+
+      
+    }
+}
